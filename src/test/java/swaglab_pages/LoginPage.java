6dea83b5c000574e9b14bf2399f7881d2190cb94
niharika2807 <niharika.nc04@gmail.com>
2025-07-19 14:02:34 +0530

    Initial commit - Swag Labs TestNG login suite

--- src/test/java/swaglab_pages/LoginPage.java
+++ src/test/java/swaglab_pages/LoginPage.java
@@ -1,43 +1,20 @@
 package swaglab_pages;
 
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import swaglab_test.BaseClass;
 
 public class LoginPage {
+    WebDriver driver;
 
-	WebDriver driver = BaseClass.driver;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
-	// ====================== Lsinssssocators =========================
-
-	@FindBy(xpath = "//input[@id='user-name']")
-	WebElement UserName;
-
-	@FindBy(xpath = "//input[@id='password']")
-	WebElement Password;
-
-	@FindBy(xpath = "//input[@id='login-button']")
-	WebElement LoginBtn;
-
-	// ======================= Methods ======================
-	
-	// constructor to initialize all the web elements 
-	public LoginPage() {
-		
-		PageFactory.initElements(driver, this);
-
-	}
-
-	public void LoginFunction(String UserNameVal, String PasswordVal) {
-
-		UserName.sendKeys(UserNameVal);
-		Password.sendKeys(PasswordVal);
-		LoginBtn.click();
-
-	}
-
+    public void LoginFunction(String username, String password) {
+        driver.findElement(By.id("user-name")).clear();
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+    }
 }
--- src/test/java/swaglab_test/BaseClass.java
+++ src/test/java/swaglab_test/BaseClass.java
@@ -1,36 +1,34 @@
 package swaglab_test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.Duration;
+
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 
 public class BaseClass {
     public static WebDriver driver;
     public static XSSFWorkbook wbook;
     public static XSSFSheet sheet;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() throws IOException {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
 
-        FileInputStream fis = new FileInputStream("exceldata.xlsx");
+        // Excel Load (used in Excel test case)
+        FileInputStream fis = new FileInputStream("src/test/resources/exceldata.xlsx");
         wbook = new XSSFWorkbook(fis);
         sheet = wbook.getSheet("data");
-        if (sheet == null) {
-            throw new IllegalStateException("Sheet named 'data' not found!");
-        }
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown() throws IOException {
         wbook.close();
         driver.quit();
